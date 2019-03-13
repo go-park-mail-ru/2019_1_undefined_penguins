1132951dc5b7f8db25324c2a002fc6579710c62b
Betchika99 <dobrl2011@mail.ru>
2019-03-13 19:26:49 +0300

    [FIX] Updated leaderboards

--- iternal/pkg/controllers/leaderboard.go
+++ iternal/pkg/controllers/leaderboard.go
@@ -7,11 +7,20 @@ import (
 )
 
 func GetLeaders(w http.ResponseWriter, r *http.Request) {
-	fmt.Print(r)
-	bytes, err := json.Marshal(users)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error":"=("}`))
+	if r.Method == "OPTIONS" {
+		setupResponse(&w, r)
+		w.WriteHeader(200)
+		return
+	} else {
+		fmt.Print(r)
+		bytes, err := json.Marshal(users)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"error":"=("}`))
+		}
+		setupResponse(&w, r)
+		w.Write(bytes)
+		fmt.Println(bytes)
+
 	}
-	w.Write(bytes)
 }
