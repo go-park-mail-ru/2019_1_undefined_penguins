436bf550c688c72e7b8f3045cdcc37197956a98f
Ваше Имя <kirilek@bk.ru>
2019-04-27 10:44:21 +0300

    added id in grpc

--- internal/app/microChat/userManager.go
+++ internal/app/microChat/userManager.go
@@ -7,16 +7,19 @@ import (
 
 type UserManager struct {
 	Login string
+	ID uint64
 }
 
 func NewUserManager() *UserManager {
 	return &UserManager{
 		Login: "",
+		ID: 0,
 	}
 }
 
 func (u *UserManager) Check(ctx context.Context, in *User) (*User, error) {
 	user, _ := database.GetUserByLogin(in.Login)
 	in.Login = user.Login
+	in.ID = uint64(user.ID)
 	return in, nil
 }
