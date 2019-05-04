a579c17f88dc825f7583b6b09a79d3ac2c64af95
Kate <kirilek@bk.ru>
2019-05-04 14:48:20 +0300

    added mew message format

--- internal/app/game/message.go
+++ internal/app/game/message.go
@@ -1,12 +1,40 @@
 package game
 
-type Message struct {
+//from back to front
+type OutcomeMessage struct {
 	Type string `json:"type"`
-	Payload PayloadMessage `json:"payload,omitempty"`
+	Payload OutPayloadMessage `json:"payload"`
 }
 
-type PayloadMessage struct {
-	Player string `json:"player"`
+type OutPayloadMessage struct {
+	Penguin PenguinMessage `json:"penguin"`
+	Bullet BulletMessage `json:"bullet"`
+}
+
+type PenguinMessage struct {
+	Name string `json:"name"`
+	Clockwise bool `json:"clockwise"`
+	Alpha int `json:"alpha"`
+}
+
+type GunMessage struct {
+	Name string `json:"name"`
+	Bullet BulletMessage `json:"bullet"`
+	Alpha int `json:"alpha"`
+}
+
+type BulletMessage struct {
+	DistanceFromCenter int `json:"distance_from_center"`
+	Alpha int `json:"alpha"`
+}
+
+//from front to back
+type IncomeMessage struct {
+	Type string `json:"type"`
+	Payload InPayloadMessage `json:"payload,omitempty"`
+}
+
+type InPayloadMessage struct {
+	Name string `json:"name"`
 	Command string `json:"command"`
 }
-
