3df884432e2f469caac9515af59f7f69b01dbd3e
Kate <kirilek@bk.ru>
2019-05-08 04:21:39 +0300

    fixed leads(?)

--- internal/pkg/controllers/leaderboard.go
+++ internal/pkg/controllers/leaderboard.go
@@ -1,31 +1,57 @@
 package controllers
 
 import (
-	db "2019_1_undefined_penguins/internal/pkg/database"
+	//db "2019_1_undefined_penguins/internal/pkg/database"
+	"2019_1_undefined_penguins/internal/pkg/helpers"
+	"2019_1_undefined_penguins/internal/pkg/models"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"google.golang.org/grpc"
 	"net/http"
 	"strconv"
+
+
+	"golang.org/x/net/context"
+	//"google.golang.org/grpc/status"
 )
 
 func GetLeaderboardPage(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
+	leaders := new(models.LeadersInfo)
 
 	if err != nil {
 		id = 1
+		leaders.ID = uint64(id)
 	}
+	leaders.ID = uint64(id)
 
-	users, _ := db.GetLeaders(id)
+	grcpConn, err := grpc.Dial(
+		"127.0.0.1:8083",
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		helpers.LogMsg("Can`t connect to grpc")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer grcpConn.Close()
+
+	authManager := models.NewAuthCheckerClient(grcpConn)
+	ctx := context.Background()
+
+	users, _ := authManager.GetUserArray(ctx, leaders)
+	//users, _ := db.GetLeaders(id)
   //TODO: fix 404!!!!
 	fmt.Println("led: ", users)
-	if len(users) == 0 {
+	if users == nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	if respBody, err := json.Marshal(users); err != nil {
+	u := users.Users
+	if respBody, err := json.Marshal(u); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	} else {
 		w.WriteHeader(http.StatusOK)
@@ -34,12 +60,31 @@ func GetLeaderboardPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetLeaderboardInfo(w http.ResponseWriter, r *http.Request) {
-	info, err := db.UsersCount()
+	grcpConn, err := grpc.Dial(
+		"127.0.0.1:8083",
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		helpers.LogMsg("Can`t connect to grpc")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer grcpConn.Close()
+
+	authManager := models.NewAuthCheckerClient(grcpConn)
+	ctx := context.Background()
+
+	//info, err := db.UsersCount()
+	info, _ := authManager.GetUserCountInfo(ctx, new(models.Nothing))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	if respBody, err := json.Marshal(info); err != nil {
+
+	var i models.LeadersInfo1
+	i.UsersOnPage = uint(info.UsersOnPage)
+	i.Count = uint(info.Count)
+	if respBody, err := json.Marshal(i); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	} else {
 		w.WriteHeader(http.StatusOK)
