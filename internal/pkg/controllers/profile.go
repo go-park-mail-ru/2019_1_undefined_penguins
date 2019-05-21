5db923bb7b038d668e9094bd1d4b1f96302d94fe
Kate <kirilek@bk.ru>
2019-05-21 14:40:00 +0300

    added user access to storage

--- internal/pkg/controllers/profile.go
+++ internal/pkg/controllers/profile.go
@@ -5,6 +5,7 @@ import (
 	"2019_1_undefined_penguins/internal/pkg/helpers"
 	"encoding/json"
 	"fmt"
+	//"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"golang.org/x/net/context"
@@ -124,13 +125,11 @@ func UploadImage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-
-	//////////////////////////
-	// The session the S3 Uploader will use
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("ru-msk"),
-		//Credentials: credentials.NewStaticCredentials("sAAbC9Cnqau5FUHy1MaTNB", "bTaRWfSQigezBT4kMgTRcdTpbox42Ske7QHDgGYxAk3y", ""),
-		Endpoint: aws.String("http://hb.bizmrg.com"),
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState:session.SharedConfigEnable,
+		Config: aws.Config{
+			Endpoint:aws.String("http://hb.bizmrg.com"),
+		},
 	}))
 	svc := s3.New(sess)
 
@@ -146,8 +145,12 @@ func UploadImage(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
 	fileName := user.Login + t.Format("20060102150405") + extension
 
+	bucket := "penguins_images"
+	acl := "public-read"
+	key := fileName
+
 	res, err := svc.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String("penguins_images"),
+		Bucket: aws.String(bucket),
 		Key:    aws.String(fileName),
 		Body:   f,
 	})
@@ -158,30 +161,53 @@ func UploadImage(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(res)
 	}
 
-	//get it back
-	res1, err := svc.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String("penguins_images"),
-		Key:    aws.String(fileName),
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	} else {
-		fmt.Println(res1)
+	params := &s3.PutObjectAclInput{
+		Bucket:&bucket,
+		ACL:&acl,
+		Key:&key,
 	}
 
-	//avatar := "https://hb.bizmrg.com/penguins_images/user1220190520235514.png"
+	_, err = svc.PutObjectAcl(params)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	newUser := new(models.User)
 	newUser = user
 	newUser.Picture = "https://hb.bizmrg.com/penguins_images/" + fileName
 	newUser.ID = user.ID
-	_, errr := models.AuthManager.ChangeUser(ctx, newUser)
-	fmt.Println(errr)
+	//_, errr := models.AuthManager.ChangeUser(ctx, newUser)
+	//fmt.Println(errr)
 	bytes, err := json.Marshal(newUser)
 	w.Write(bytes)
-	//////////////////////////
+
+	// check object grands
+	//input := &s3.GetObjectAclInput{
+	//	Bucket: aws.String("penguins_images"),
+	//	Key: aws.String("user620190521134436.png"),
+	//}
+	//
+	//result1, err := svc.GetObjectAcl(input)
+	//if err != nil {
+	//	if aerr, ok := err.(awserr.Error); ok {
+	//		switch aerr.Code() {
+	//		case s3.ErrCodeNoSuchKey:
+	//			fmt.Println(s3.ErrCodeNoSuchKey, aerr.Error())
+	//		default:
+	//			fmt.Println(aerr.Error())
+	//		}
+	//	} else {
+	//		// Print the error, cast err to awserr.Error to get the Code and
+	//		// Message from an error.
+	//		fmt.Println(err.Error())
+	//	}
+	//	return
+	//}
+	//
+	//fmt.Println(result1)
 
 
+	// saving picture in fileserver
 	// file, handler, err := r.FormFile("avatar")
 	// if err != nil {
 	// 	helpers.LogMsg("Ошибка при получении файла из тела запроса")
