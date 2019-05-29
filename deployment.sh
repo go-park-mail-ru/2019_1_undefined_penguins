9b5d6570b27c9c648d24aac9008c28f19456e1e3
Kate <kirilek@bk.ru>
2019-05-29 20:47:39 +0300

    added ci

--- deployment.sh
+++ deployment.sh
@@ -4,6 +4,7 @@ ssh-keyscan -H $PRODUCTION_MACHINE_ADDRESS >> ~/.ssh/known_hosts
 chmod 600 ./deployment_travis_key
 ssh -i ./deployment_travis_key travis@$PRODUCTION_MACHINE_ADDRESS << EOF
 
+cd /home/kate/2019_1_undefined_penguins
 echo Building... && go build cmd/main.go && \
 echo Restarting service... &&  systemctl restart penguin-backend.service && \
 echo Enabling service ... && systemctl enable penguin-backend.service && \
